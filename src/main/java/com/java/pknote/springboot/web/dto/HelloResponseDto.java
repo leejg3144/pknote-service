a22d1b05eb4b0f114ef8cdb453e61c17b0e04bc2
leejg3144 <jaosn0620@gmail.com>
2020-03-21 00:23:30 +0900

    [20.03.21] jpa실습

---
+++ src/main/java/com/java/pknote/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.java.pknote.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+
+    private final int amount;
+}
---
+++ src/main/java/com/java/pknote/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.java.pknote.springboot.web.dto;
+
+import com.java.pknote.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
